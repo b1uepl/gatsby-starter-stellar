a84c6d6446de50954966b350e83aa6646426c6bc
b1uepl <plnam@scemoblu.com>
2019-05-09 23:02:17 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,8 @@ import logo from '../assets/images/logo.svg';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <h1>NAM Pui Lam Polly</h1>
+        <p>Personable IT consultant with 4+ years expertise in a fast-paced tech firm. Seeking to advance my career by growing with the team.</p>
     </header>
 )
 
